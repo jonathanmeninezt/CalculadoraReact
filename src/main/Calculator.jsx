7e49bcab77a968eda8600110c25e4c9ec33ac6f5
Jonathan Meninez <116603241+jonathanmeninezt@users.noreply.github.com>
2023-05-24 20:45:33 -0300

    criação- calculadora- react / calculator.jsx / react

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import "./Calculator.css";
+
+import React, { Component } from "react";
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator">
+        <h1>Ola</h1>
+      </div>
+    );
+  }
+}
